e296a0bdf7b833e39be9263641ac554eae2f9a45
Jaay7 <jayakrishna.madem@gmail.com>
2022-03-23 08:17:59 +0530

    added react-navigation

--- App.js
+++ App.js
@@ -1,14 +1,39 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SignupScreen from './pages/SignupScreen';
+import LoginScreen from './pages/LoginScreen';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Signup"
+      >
+        <Stack.Screen 
+          name="Signup" 
+          component={SignupScreen} 
+          options={{
+            headerShown: false
+          }}
+        />
+        <Stack.Screen 
+          name="Login" 
+          component={LoginScreen} 
+          options={{
+            headerShown: false
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
