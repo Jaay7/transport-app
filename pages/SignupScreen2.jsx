10a32bdebe4357ab19c14f4b922500a6b017d449
Jaay7 <jayakrishna.madem@gmail.com>
2022-06-17 09:15:19 +0530

    signup for driver

--- pages/SignupScreen2.jsx
+++ pages/SignupScreen2.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, StatusBar, StyleSheet } from 'react-native'
+import { View, StatusBar, StyleSheet, ScrollView } from 'react-native'
 import { Flex, HStack, VStack, Text, TextInput, Button } from '@react-native-material/core'
 import { Ionicons } from '@expo/vector-icons';
 import axios from 'axios';
@@ -7,12 +7,22 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const SignupScreen2 = ({route, navigation}) => {
   const { userType, name, email, password, mobile } = route.params;
+  // dealer
   const [natureOfMaterial, setNatureOfMaterial] = React.useState('');
   const [weightOfMaterial, setWeightOfMaterial] = React.useState('');
   const [quantity, setQuantity] = React.useState('');
   const [state, setState] = React.useState('');
   const [city, setCity] = React.useState('');
   const [loading, setLoading] = React.useState(false);
+  // driver
+  const [age, setAge] = React.useState();
+  const [truckNumber, setTruckNumber] = React.useState('');
+  const [truckCapacity, setTruckCapacity] = React.useState();
+  const [transporterName, setTransporterName] = React.useState('');
+  const [drivingExperience, setDrivingExperience] = React.useState();
+  const [route1, setRoute1] = React.useState('');
+  const [route2, setRoute2] = React.useState('');
+  const [route3, setRoute3] = React.useState('');
 
   const registerDealer = () => {
     setLoading(true);
@@ -40,14 +50,38 @@ const SignupScreen2 = ({route, navigation}) => {
   }
 
   const registerDriver = () => {
-    console.log(userType, name, email, password, mobile, state, city)
+    setLoading(true);
+    axios.post('https://transport-backend-apis.herokuapp.com/api/driver/addDriver', {
+      name: name,
+      email: email,
+      password: password,
+      age: age,
+      truckNumber: truckNumber,
+      mobile: mobile,
+      truckCapacity: truckCapacity,
+      transporterName: transporterName,
+      drivingExperience: drivingExperience,
+      route1: route1,
+      route2: route2,
+      route3: route3
+    })
+    .then(res => {
+      console.log(res);
+      AsyncStorage.setItem('userType', 'Driver');
+      AsyncStorage.setItem('userId', res.data.id.toString());
+      navigation.navigate('DriverPages');
+      setLoading(false);
+    })
+    .catch(err => {
+      console.log(err)
+    })
   }
   
   return (
     <View style={styles.container}>
       <StatusBar />
       <VStack justify="center" m={16} spacing={2}>
-        <View>
+        <ScrollView>
           <HStack items="center">
             <Ionicons 
               name="arrow-back-circle" 
@@ -106,48 +140,64 @@ const SignupScreen2 = ({route, navigation}) => {
               label="Age" 
               color='black'
               style={{ marginTop: 16 }} 
+              value={age}
+              onChangeText={(text) => setAge(text)}
             />
             <TextInput 
               variant="standard" 
               label="Truck Number" 
               color='black'
               style={{ marginTop: 16 }} 
+              value={truckNumber}
+              onChangeText={(text) => setTruckNumber(text)}
             />
             <TextInput 
               variant="standard" 
               label="Truck Capacity" 
               color='black'
               style={{ marginTop: 16 }} 
+              value={truckCapacity}
+              onChangeText={(text) => setTruckCapacity(text)}
             />
             <TextInput 
               variant="standard" 
               label="Transporter Name" 
               color='black'
               style={{ marginTop: 16 }} 
+              value={transporterName}
+              onChangeText={(text) => setTransporterName(text)}
             />
             <TextInput 
               variant="standard" 
               label="Driving Experience" 
               color='black'
               style={{ marginTop: 16 }} 
+              value={drivingExperience}
+              onChangeText={(text) => setDrivingExperience(text)}
             />
             <TextInput 
               variant="standard" 
               label="Route1" 
               color='black'
               style={{ marginTop: 16 }} 
+              value={route1}
+              onChangeText={(text) => setRoute1(text)}
             />
             <TextInput 
               variant="standard" 
               label="Route2" 
               color='black'
               style={{ marginTop: 16 }} 
+              value={route2}
+              onChangeText={(text) => setRoute2(text)}
             />
             <TextInput 
               variant="standard" 
               label="Route3" 
               color='black'
               style={{ marginTop: 16 }} 
+              value={route3}
+              onChangeText={(text) => setRoute3(text)}
             />
           </>
           }
@@ -166,7 +216,7 @@ const SignupScreen2 = ({route, navigation}) => {
             <Text variant="body2" color="gray">Already have an account?</Text>
             <Text variant="body2" onPress={() => navigation.navigate('Login')}> Login</Text>
           </HStack>
-        </View>
+        </ScrollView>
       </VStack>
     </View>
   )
